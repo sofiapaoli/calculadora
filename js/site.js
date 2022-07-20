5a68a31cde5320d00e0a77f5a441025e9478c95b
sofiapaoli <sofiapaoli@cear.ufpb.br>
2022-07-20 05:23:04 -0300

    index

--- js/site.js
+++ js/site.js
@@ -26,36 +26,45 @@ function finalizarCalculo() {
 
     // Pergunta 2 
     var eletricidade = $('#input-pergunta-2').val();
-    $('#resultado-limpo-2').html("X.XX");
-    $('#resultado-sujo-2').html((0.90286575 + 0.00089881 * eletricidade).toFixed(3));
+    var resultadoSujoEletricidade = (((186*eletricidade + 52.4)*1000).toFixed(2));
+    $('#resultado-limpo-2').html(((0.035*eletricidade).toFixed(2)));
+    $('#resultado-sujo-2').html(resultadoSujoEletricidade);
 
     // Pergunta 3 
     var gas = $('#input-pergunta-3').val();
+    var resultadoSujoGas = (((0.482*gas + 1.32)*1000).toFixed(2));
     $('#resultado-limpo-3').html("X.XX");
-    $('#resultado-sujo-3').html((1.03610667+ 0.00808571 * gas).toFixed(3));
+    $('#resultado-sujo-3').html(resultadoSujoGas);
 
     // Pergunta 4 
     var residuos = $('#input-pergunta-4').val();
-    $('#resultado-limpo-4').html("X.XX");
-    $('#resultado-sujo-4').html("X.XX");
+    var resultadoSujoResiduos = (((1.93*residuos + 3.14)*1000).toFixed(2));
+    $('#resultado-limpo-4').html(((4/1000)*residuos).toFixed(2));
+    $('#resultado-sujo-4').html(resultadoSujoResiduos);
 
     // Pergunta 5
     var transporteIndividual = $('#input-pergunta-5').val();
+    var resultadoSujoTransporteIndividual = (((200*transporteIndividual + 600).toFixed(2)));
     $('#resultado-limpo-5').html("X.XX");
-    $('#resultado-sujo-5').html("X.XX");
+    $('#resultado-sujo-5').html(resultadoSujoTransporteIndividual);
 
     // Pergunta 6 
+  
+
     var transporteColetivo = $('#input-pergunta-6').val();
+    var resultadoSujoTransporteColetivo = (((2*transporteColetivo + 13)*1000).toFixed(2));
     $('#resultado-limpo-6').html("X.XX");
-    $('#resultado-sujo-6').html((1.03610667+ 0.00808571 * transporteColetivo).toFixed(3));
+    $('#resultado-sujo-6').html(resultadoSujoTransporteColetivo);
 
     // Pergunta 7
+
     var carne = $('#input-pergunta-7').val();
+    var resultadoSujoCarne = ((0.0664*carne + 4.82)*1000).toFixed(2);
     $('#resultado-limpo-7').html("X.XX");
-    $('#resultado-sujo-7').html("X.XX");
+    $('#resultado-sujo-7').html(resultadoSujoCarne);
 
     // Diferença
-    $('#diferenca').html('X.XXX')
+    $('#diferenca').html(($('#resultado-sujo-7')+$('#resultado-sujo-6')+$('#resultado-sujo-5')+$('#resultado-sujo-4')+$('#resultado-sujo-3')+$('#resultado-sujo-2'))-($('#resultado-limpo-7')+$('#resultado-limpo-6')+$('#resultado-limpo-5')+$('#resultado-limpo-4')+$('#resultado-limpo-3')+$('#resultado-limpo-2')))
 
     UIkit.modal("#modal-resultados").show();
 }

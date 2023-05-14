679934397bb08acc5ea025d8b0e7090f752b47d8
sofiapaoli <68301143+sofiapaoli@users.noreply.github.com>
2023-05-14 12:03:43 -0300

    site.js

--- js/site.js
+++ js/site.js
@@ -26,27 +26,27 @@ function finalizarCalculo() {
 
     // Pergunta 2 
     var eletricidade = $('#input-pergunta-2').val();
-    var resultadoSujoEletricidade = (((0.0697*eletricidade  - 0.00274)).toFixed(2));
+    var resultadoSujoEletricidade = parseFloat((((0.0697*eletricidade  - 0.00274)).toFixed(2)));
     $('#resultado-limpo-2').html(((0.035*eletricidade).toFixed(2)));
     $('#resultado-sujo-2').html(resultadoSujoEletricidade);
 
     // Pergunta 3 
     var gas = $('#input-pergunta-3').val();
-    var resultadoSujoGas = (((35.89*gas)).toFixed(2));
-    var resultadoLimpoGas = ((((3.5/1000)*605.8)*gas).toFixed(2));
+    var resultadoSujoGas = parseFloat((((35.89*gas)).toFixed(2)));
+    var resultadoLimpoGas = parseFloat(((((3.5/1000)*605.8)*gas).toFixed(2)));
     $('#resultado-limpo-3').html(resultadoLimpoGas);
     $('#resultado-sujo-3').html(resultadoSujoGas);
 
     // Pergunta 4 
     var residuos = $('#input-pergunta-4').val();
-    var resultadoSujoResiduos = ((((0.03*residuos)*1000)*pessoas).toFixed(2));
+    var resultadoSujoResiduos = parseFloat(((((0.03*residuos)*1000)*pessoas).toFixed(2)));
     $('#resultado-limpo-4').html((((4/1000)*residuos)*pessoas).toFixed(2));
     $('#resultado-sujo-4').html(resultadoSujoResiduos);
 
     // Pergunta 5
     var transporteIndividual = $('#input-pergunta-5').val();
-    var resultadoSujoTransporteIndividual = (((2.84*transporteIndividual).toFixed(2)));
-    var resultadoLimpoTransporteIndividual = ((((1/1000)*transporteIndividual).toFixed(2)));
+    var resultadoSujoTransporteIndividual = parseFloat((((2.84*transporteIndividual).toFixed(2))));
+    var resultadoLimpoTransporteIndividual = parseFloat(((((1/1000)*transporteIndividual).toFixed(2))));
     $('#resultado-limpo-5').html(resultadoLimpoTransporteIndividual);
     $('#resultado-sujo-5').html(resultadoSujoTransporteIndividual);
 
@@ -54,8 +54,8 @@ function finalizarCalculo() {
   
 
     var transporteColetivo = $('#input-pergunta-6').val();
-    var resultadoSujoTransporteColetivo = ((0.32*transporteColetivo).toFixed(2));
-    var resultadoLimpoTransporteColetivo = (((1.90/1000)*transporteColetivo).toFixed(2));
+    var resultadoSujoTransporteColetivo = parseFloat(((0.32*transporteColetivo).toFixed(2)));
+    var resultadoLimpoTransporteColetivo = parseFloat((((1.90/1000)*transporteColetivo).toFixed(2)));
     $('#resultado-limpo-6').html(resultadoLimpoTransporteColetivo);
     $('#resultado-sujo-6').html(resultadoSujoTransporteColetivo);
 
@@ -66,7 +66,7 @@ function finalizarCalculo() {
     $('#resultado-sujo-7').html(resultadoSujoCarne);
 
     // Diferença
-    $('#diferenca').html(($('#resultado-sujo-7')+$('#resultado-sujo-6')+$('#resultado-sujo-5')+$('#resultado-sujo-4')+$('#resultado-sujo-3')+$('#resultado-sujo-2'))-($('#resultado-limpo-7')+$('#resultado-limpo-6')+$('#resultado-limpo-5')+$('#resultado-limpo-4')+$('#resultado-limpo-3')+$('#resultado-limpo-2')))
+    $('#diferenca').html(resultadoSujoEletricidade+resultadoSujoGas+resultadoSujoResiduos+resultadoSujoTransporteIndividual+resultadoSujoTransporteColetivo-resultadoLimpoGas-resultadoLimpoTransporteIndividual-resultadoLimpoTransporteColetivo);
 
     UIkit.modal("#modal-resultados").show();
 }
